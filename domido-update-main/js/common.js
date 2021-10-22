7cc4d2ca60ff5cd16f40151d77b7381ff45cee4a
Artem Puryshev <dvrart@gmail.com>
2021-10-22 17:11:03 +0200

    start

---
+++ domido-update-main/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.burger').click(function(){
+        $('.header.header .col-md-9').addClass('show');
+    });
+    $('.closemenu').click(function(){
+        $('.header.header .col-md-9').removeClass('show');
+    });
+
+
+
+    
+});
